bdd9c6ed457bae53a1b9133dbc317ca49ff2a0b4
SarathAdhi <s.sarathadhithya@gmail.com>
2022-02-13 21:28:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.on("messageCreate", msg => {
 
     if (msg.author.bot == false && str.includes("!")){   //reply only if the user and not the bot itself
         
-        if (str.includes("prog")) {
+        if (/prog/.test(str)) {
             msg.reply(chatReply());
         }
         if (str.includes("help")) {
